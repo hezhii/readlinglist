8ba2e297bbecbb2d0bbc2a679b7951906f6ffd61
biu <hezhou920@gmail.com>
2018-11-15 17:23:48 +0800

    定制应用程序错误页面

--- src/main/java/com/whezh/readinglist/ReadingListController.java
+++ src/main/java/com/whezh/readinglist/ReadingListController.java
@@ -1,10 +1,13 @@
 package com.whezh.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.security.Principal;
 import java.util.List;
@@ -44,4 +47,15 @@ public class ReadingListController {
     public String loginPage() {
         return "login";
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/fail")
+    public void fail() {
+        throw new RuntimeException();
+    }
+
+//    @ExceptionHandler(value = RuntimeException.class)
+//    @ResponseStatus(value = HttpStatus.BANDWIDTH_LIMIT_EXCEEDED)
+//    public String error() {
+//        return "error";
+//    }
 }
